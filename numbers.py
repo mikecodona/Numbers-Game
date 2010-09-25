07b23ea5c42222848edaccfb78dac4c636971f3b
Mike Codona <mikecodona@googlemail.com>
2010-09-25 19:24:48 +0100

    Moved functions into a class on there own

--- numbers.py
+++ numbers.py
@@ -1,66 +1,107 @@
 from datetime import datetime
+import sys
 
-def evaltree(tree, i=1):
-    if type(tree[i]) == type(1):
-        return tree[i]
-    elif tree[i] == '+':
-        return evaltree(tree, left(i)) + evaltree(tree, right(i))
-    elif tree[i] == '-':
-        return evaltree(tree, left(i)) - evaltree(tree, right(i))
-    elif tree[i] == '*':
-        return evaltree(tree, left(i)) * evaltree(tree, right(i))
-    elif tree[i] == '/':
-        right_val = evaltree(tree, right(i))
-        if right_val > 0:
-            return evaltree(tree, left(i)) / right_val
+class InvalidTreeError(Exception):
+    pass
+
+class Solution():
+
+    s_value = None 
+
+    def __init__(self, numbers, tree=None):
+        self.numbers = numbers
+        self.tree = tree
+
+    # value is memorized to avoid repeating calculation
+    def value(self):
+        if not self.s_value:
+            self.s_value = self.evaltree()
+        
+        return self.s_value
+
+    def evaltree(self, i=1):
+        current_node = self.tree[i]
+
+        if type(current_node) == type(1):
+            return current_node 
+        elif current_node == '+':
+            return self.evaltree(left(i)) + self.evaltree(right(i))
+        elif current_node == '-':
+            return self.evaltree(left(i)) - self.evaltree(right(i))
+        elif current_node == '*':
+            return self.evaltree(left(i)) * self.evaltree(right(i))
+        elif current_node == '/':
+            left_val = self.evaltree(left(i))
+            right_val = self.evaltree(right(i))
+            if right_val > 0 and left_val % right_val == 0:
+                return left_val / right_val
+            else:
+                raise InvalidTreeError('Invalid solution tree')
         else:
-            return 0
-    else:
-        print "Doing something wrong"
+            raise Exception('Malformed solution tree')
 
-def strtree(tree, i=1):
-    if type(tree[i]) == type(1):
-        return str(tree[i])
-    elif tree[i] in ['+', '-', '*', '/']:
-        return "(" + strtree(tree, left(i)) + " " + tree[i] + " " + strtree(tree, right(i)) + ")"
-    else:
-        print "Doing something wrong"
+    def __str__(self):
+        return self.strtree() + " = " + str(self.value())
 
-""" Takes a tree as input and returns a list of new trees """
-""" expand([None, 1], [2]) returns [None, '+', 1, 2] """
-def expand(solution):
-    tree, numbers = solution
+    def strtree(self, i=1):
+        current_node = self.tree[i]
+        
+        if type(current_node) == type(1):
+            return str(current_node)
+        elif current_node in ['+', '-', '*', '/']:
+            return "(" + self.strtree(left(i)) + " " + current_node + " " + self.strtree(right(i)) + ")"
+        else:
+            print "Doing something wrong"
 
-    new_solutions = []
-    operators = ['+', '-', '*', '/']
+    """ Takes a tree as input and returns a list of new trees """
+    """ expand([None, 1], [2]) returns [None, '+', 1, 2] """
+    def expand(self):
+        solutions = []
+        operators = ['+', '-', '*', '/']
 
+        # Generate initial set of Solutions
+        if self.tree == None:
+            for n in self.numbers:
+                numbers = self.numbers[:]
+                numbers.remove(n)
+                solutions.append(Solution(numbers, [None, n]))
+            
+            return solutions
 
-    leaves = []
-    for i in xrange(len(tree)):
-        if type(tree[i]) == type(1):
-             leaves.append(i)
+        # Find all leaf nodes
+        leaves = []
+        for i in xrange(len(self.tree)):
+            if type(self.tree[i]) == type(1):
+                 leaves.append(i)
 
+        # Expand current tree
+        for l in leaves:
+            for o in operators:
+                for n in self.numbers: 
+                    tree = self.tree[:]
+                    tree.extend([None] * (len(self.tree) + 1))
 
-    for l in leaves:
-        for o in operators:
-            for n in numbers: 
-                # Cannot divide if remainder is non-zero
-                if o == '/' and tree[l] % n > 0:
-                    continue
+                    tree[l] = o 
+                    tree[left(l)] = self.tree[l]
+                    tree[right(l)] = n 
 
-                new_tree = tree[:]
-                new_tree.extend([None] * (len(tree) + 1))
+                    numbers = self.numbers[:]
+                    numbers.remove(n)
 
-                new_tree[l] = o 
-                new_tree[left(l)] = tree[l]
-                new_tree[right(l)] = n 
+                    solution = Solution(numbers, tree)
+                    
+                    # Calculate value of new solution.
+                    # We catch any exception in evaluating
+                    # the solution, this removes divide by
+                    # 0 or solutions which depend on non-integer
+                    # division.
+                    try:
+                        solution.value()
+                        solutions.append(solution)
+                    except InvalidTreeError as e:
+                        pass
 
-                new_numbers = numbers[:]
-                new_numbers.remove(n)
-
-                new_solutions.append((new_tree, new_numbers))
-
-    return new_solutions
+        return solutions
 
 def left(i):
     return i * 2
@@ -69,34 +110,43 @@ def right(i):
     return i * 2 + 1
             
 def main():
-    findsolution([1, 2, 5, 10, 20, 50], 121)
+    if len(sys.argv) == 8:
+        numbers = map(int, sys.argv[1:7])
+        target = int(sys.argv[7])
+
+        print numbers, target
+        findsolution(numbers, target)
+
 
 """ Find solution takes a list of numbers and tries to find a solution """
 def findsolution(numbers, target):
-    solutions = []
-    # Generate initial set of 'Solutions
-    for n in numbers:
-        solution = ([None, n], numbers[:])
-        solution[1].remove(n) 
-        solutions.append(solution)
+    numbers.reverse()
+    solutions = Solution(numbers).expand()
 
-    print solutions
+    best = solutions[0]
 
     start = datetime.now()
      
-    while (datetime.now() - start).seconds < 10:
+    while (datetime.now() - start).seconds < 29:
+        solutions.sort(key=lambda sol: distance(sol.value(), target))
+        print solutions[0]
+
+        if solutions[0].value() == target:
+            return
+
+        # Only work on the best 100 solutions
+        if len(solutions) > 1000:
+            solutions = solutions[:1000]
+        
         new = []
         for s in solutions:
-            new.extend(expand(s))
-   
-        for tree, numbers in new:
-            print strtree(tree), evaltree(tree), numbers #, eval(strtree(n))
+            new.extend(s.expand())
 
         solutions.extend(new)
 
 
-def distance(x, target):
-    return abs(x-target)
+def distance(value, target):
+    return abs(value - target)
 
 if __name__ == '__main__':
     main()
